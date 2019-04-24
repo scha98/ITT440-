fed9eb568fe8ceb02c9929eddec8dd683ca39d16
Sharifah Hasyimah <shahifq12@gmail.com>
2019-04-24 19:43:49 +0800

    4

---
+++ socket/server.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class server {
+
+
+	public static void main (String[] args) throws IOException{
+
+	  ServerSocket syima=new ServerSocket(7000);
+	Socket shahfiq=syima.accept();
+     System.out.println ("client connected");
+     InputStreamReader gt= new 
+	InputStreamReader(shahfiq.getInputStream());
+	BufferedReader ot = new BufferedReader(gt);
+
+ 	
+       PrintWriter rp= new PrintWriter(shahfiq.getOutputStream());
+       rp.println("whatup");
+       rp.flush();
+
+ 	String Strg =ot.readLine ();
+	System.out.println(Strg);
+
+
+}	
+}
